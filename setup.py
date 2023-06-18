a4c8971b4a010e452f610cb9404bc76f4a9d60ba
karmataco <132534445+karmataco@users.noreply.github.com>
2023-06-18 01:57:59 -0700

    small messaging tweaks

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'extra_streamlit_components',
         'streamlit_option_menu',
         'trycourier',
-        'streamlit_cookies_manager',
+        'git+https://github.com/vidz1979/streamlit-cookies-manager.git',
         'argon2-cffi',
         'deta'
     ],
